0db37572f8e4da7b8670e2365c302dbbe16466f7
Emanuel Imhof <emanuel.imhof@twygmbh.ch>
2013-03-11 10:23:37 +0100

    Removed .idea from git.

---
+++ lib/railslove/acts/birthday/adapter/sqlite3_adapter.rb
@@ -0,0 +1,12 @@
+# Birthday
+require 'railslove/acts/birthday/adapter/sqlite_adapter'
+module Railslove
+  module Acts #:nodoc:
+    module Birthday #:nodoc:
+      module Adapter
+        class SQLite3Adapter < SQLiteAdapter
+        end
+      end
+    end
+  end
+end
