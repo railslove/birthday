f48e73bf051a9ce5192bee3a842b45b689f470da
Ken Hill <ken@ted.com>
2015-09-26 13:42:06 -0400

    update specs to avoid deprecation warnings

--- spec/birthday_spec.rb
+++ spec/birthday_spec.rb
@@ -22,41 +22,41 @@ describe Marriage do
 
   it 'check anniversary_today? correctly' do
     time_travel_to Date.parse('2011-09-09')
-    @m1.should be_anniversary_today
-    @m2.should_not be_anniversary_today
+    expect(@m1).to be_anniversary_today
+    expect(@m2).not_to be_anniversary_today
   end
 
   it 'check anniversary_age correctly' do
     time_travel_to Date.parse('2011-09-08')
-    @m1.anniversary_age.should eq(1)
-    @m2.anniversary_age.should eq(2)
+    expect(@m1.anniversary_age).to eq(1)
+    expect(@m2.anniversary_age).to eq(2)
   end
 
   it 'finds people\'s by anniversary date' do
     time_travel_to Date.parse('2011-08-08')
     anniversaries_today = Marriage.find_anniversaries_for(Date.today)
-    anniversaries_today.should_not include(@m1)
-    anniversaries_today.should include(@m2)
+    expect(anniversaries_today).not_to include(@m1)
+    expect(anniversaries_today).to include(@m2)
   end
 
   it 'finds people\'s by anniversary date - dumb scope' do
     time_travel_to Date.parse('2011-08-08')
     anniversaries_today = Marriage.anniversary_today
-    anniversaries_today.should_not include(@m1)
-    anniversaries_today.should include(@m2)
+    expect(anniversaries_today).not_to include(@m1)
+    expect(anniversaries_today).to include(@m2)
   end
 
   it 'finds people\'s by anniversary dates' do
     anniversaries = Marriage.find_anniversaries_for(Date.parse('2011-08-01'), Date.parse('2011-12-12'))
-    anniversaries.should include(@m1)
-    anniversaries.should include(@m2)
+    expect(anniversaries).to include(@m1)
+    expect(anniversaries).to include(@m2)
   end
 
   it 'finds people\'s by anniversary dates with years overlapping' do
     anniversaries = Marriage.find_anniversaries_for(Date.parse('2011-09-01'), Date.parse('2012-08-12'))
-    anniversaries.should include(@m1)
-    anniversaries.should include(@m2)
-    anniversaries.should_not include(@m3)
+    expect(anniversaries).to include(@m1)
+    expect(anniversaries).to include(@m2)
+    expect(anniversaries).not_to include(@m3)
   end
 end
 
@@ -74,40 +74,40 @@ describe Person do
 
   it 'check birthday_today? correctly' do
     time_travel_to Date.parse('2011-09-09')
-    @p1.should be_birthday_today
-    @p2.should_not be_birthday_today
+    expect(@p1).to be_birthday_today
+    expect(@p2).not_to be_birthday_today
   end
 
   it 'check birthday_age correctly' do
     time_travel_to Date.parse('2011-09-08')
-    @p1.birthday_age.should eq(4)
-    @p2.birthday_age.should eq(7)
+    expect(@p1.birthday_age).to eq(4)
+    expect(@p2.birthday_age).to eq(7)
   end
 
   it 'finds people\'s by birthday date' do
     time_travel_to Date.parse('2011-08-08')
     birthdays_today = Person.find_birthdays_for(Date.today)
-    birthdays_today.should_not include(@p1)
-    birthdays_today.should include(@p2)
+    expect(birthdays_today).not_to include(@p1)
+    expect(birthdays_today).to include(@p2)
   end
 
   it 'finds people\'s by birthday date - dumb scope' do
     time_travel_to Date.parse('2011-08-08')
     birthdays_today = Person.birthday_today
-    birthdays_today.should_not include(@p1)
-    birthdays_today.should include(@p2)
+    expect(birthdays_today).not_to include(@p1)
+    expect(birthdays_today).to include(@p2)
   end
 
   it 'finds people\'s by birthday dates' do
     birthdays = Person.find_birthdays_for(Date.parse('2011-08-01'), Date.parse('2011-12-12'))
-    birthdays.should include(@p1)
-    birthdays.should include(@p2)
+    expect(birthdays).to include(@p1)
+    expect(birthdays).to include(@p2)
   end
 
   it 'finds people\'s by birthday dates with years overlapping' do
     birthdays = Person.find_birthdays_for(Date.parse('2011-09-01'), Date.parse('2012-08-12'))
-    birthdays.should include(@p1)
-    birthdays.should include(@p2)
-    birthdays.should_not include(@p3)
+    expect(birthdays).to include(@p1)
+    expect(birthdays).to include(@p2)
+    expect(birthdays).not_to include(@p3)
   end
 end
